7cb7beba63fe4ff7e6a0002693b9a82047ee0d4f
radhika <radhika.vitvel29@gmail.com>
2026-06-30 15:32:54 +0530

    standalone jwt test

---
+++ backend/testjwt.py
@@ -0,0 +1,22 @@
+import jwt
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlJhZGhpa2EiLCJyb2xlIjoiaXQgc3R1ZGVudCJ9.oT5t0AeM_GondPYnp6MO-aPqy4sS9E9riXfIpAGihv8"
+secret = "a-string-secret-at-least-256-bits-long"
+secret1="false-secret"
+#without verification
+unverified = jwt.decode(token, options={"verify_signature": False})
+print("Unverified payload:")
+print(unverified)
+
+#with verification(wrong secret)
+try:
+    verified = jwt.decode(token, secret1, algorithms=["HS256"])
+    print("\nVERIFIED! Payload:")
+    print(verified)
+except jwt.InvalidSignatureError:
+    print("\nVERIFICATION FAILED!! SIGNATURE DOESN'T MATCH!")
+try:
+    verified = jwt.decode(token, secret, algorithms=["HS256"])
+    print("\nVERIFIED! Payload:")
+    print(verified)
+except jwt.InvalidSignatureError:
+    print("\nVERIFICATION FAILED!! SIGNATURE DOESN'T MATCH!")
